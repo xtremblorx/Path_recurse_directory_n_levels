9d44fc6662ff6e09b21d0bb5ee5c88bbc06d6fca
linux-printer-troubleshooter <117502862+linux-printer-troubleshooter@users.noreply.github.com>
2023-02-09 22:13:20 -0800

    Add files via upload

---
+++ echo_me.bat
@@ -0,0 +1,7 @@
+mode 30,10
+color 3b
+POWERSHELL -C "wRITE-HOST \"`tif match != None: \"">>"c:\USERS\ANIL BAPNA\DESKTOP\PYTHON.SCRIPT.PY"
+color 5c
+ping -n  1 localhost
+ping -n  1 localhost 
+ping -n  1 localhost
