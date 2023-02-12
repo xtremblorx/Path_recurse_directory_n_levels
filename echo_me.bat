08c0a69675295c96712e6ec2cde652dbd57a4707
linux-printer-troubleshooter <117502862+linux-printer-troubleshooter@users.noreply.github.com>
2023-02-12 20:19:58 +0530

    Update echo_me.bat

--- echo_me.bat
+++ echo_me.bat
@@ -1,6 +1,6 @@
 mode 30,10
 color 3b
-POWERSHELL -C "wRITE-HOST \"`tif match != None: \"">>"c:\USERS\ANIL BAPNA\DESKTOP\PYTHON.SCRIPT.PY"
+POWERSHELL -C "wRITE-HOST \"`tif match != None: \"">>"%ptr%\PYTHON.SCRIPT.PY"
 color 5c
 ping -n  1 localhost
 ping -n  1 localhost 
